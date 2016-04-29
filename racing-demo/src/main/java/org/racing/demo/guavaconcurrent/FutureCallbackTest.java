2454cffb6bbcc39a9d985f589c46e1cede68fd9a
dennis <dcydwj@126.com>
2016-04-29 08:31:17 +0800

    guava concurrent ExecutorService test

---
+++ racing-demo/src/main/java/org/racing/demo/guavaconcurrent/FutureCallbackTest.java
@@ -0,0 +1,37 @@
+package org.racing.demo.guavaconcurrent;
+
+import com.google.common.base.Throwables;
+import com.google.common.util.concurrent.*;
+
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2016/4/28.
+ */
+public class FutureCallbackTest {
+    public static void main(String[] args) {
+        ListeningExecutorService listeningExecutorService = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
+        ListenableFuture<String> future = listeningExecutorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(10);
+            return UUID.randomUUID().toString();
+        });
+        System.out.println("1111");
+        Futures.addCallback(future, new FutureCallback<String>() {
+            @Override
+            public void onSuccess(String result) {
+                System.out.println(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Throwables.propagate(t);
+            }
+        });
+        System.out.println("2222");
+        listeningExecutorService.shutdown();
+        System.out.println("3333");
+    }
+}
