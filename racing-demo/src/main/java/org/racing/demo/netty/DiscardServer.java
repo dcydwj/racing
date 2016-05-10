71a21c146f10d0807e760d0dfe5eaaa056c9a70d
dennis <dcydwj@126.com>
2016-05-10 09:05:13 +0800

    netty discard

---
+++ racing-demo/src/main/java/org/racing/demo/netty/DiscardServer.java
@@ -0,0 +1,12 @@
+package org.racing.demo.netty;
+
+/**
+ * Created by dennis on 2016/5/10.
+ */
+public class DiscardServer {
+    private int port;
+
+    public DiscardServer(int port) {
+        this.port = port;
+    }
+}
