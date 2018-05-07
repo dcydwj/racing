e43349d5e5a35c4ab7af497012882ca6324af728
congying.deng <congying.deng@ymm56.com>
2018-05-07 13:49:11 +0800

    test idea merge

---
+++ racing-seda/src/test/java/org/racing/seda/A.java
@@ -0,0 +1,12 @@
+package org.racing.seda;
+
+/**
+ * @author congying.deng
+ * @Description:
+ * @date 2018/5/7 13:48
+ */
+public class A {
+    public static void main(String[] args) {
+        System.out.println("test merge");
+    }
+}
